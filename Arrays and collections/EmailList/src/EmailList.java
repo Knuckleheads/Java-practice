f337b371aa2385a8381b7102b344a55628629720
Oleg Alekseev <26knuckleheds08@gmail.com>
2022-04-04 12:51:33 +0700

    init

---
+++ Arrays and collections/EmailList/src/EmailList.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class EmailList {
+    public static TreeSet<String> emailsTreeSet = new TreeSet<>() {{
+        add("kostya@gmail.com");
+        add("qwer123@yandex.ru");
+        add("sobaka@mail.ru");
+    }};
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите команду: ");
+            String input = scanner.nextLine();
+            String splitCommand[] = input.split("\s", 2);
+
+            switch (splitCommand[0].toLowerCase()) {
+                case "list":
+                    printEmails();
+                    break;
+
+                case "add":
+                    if (splitCommand.length < 2) {
+                        System.out.println("Не введен email");
+                        break;
+                    }
+                    String mail = splitCommand[1];
+                    if (mail.matches("[^@]+@[^\\.\\,]+\\..+"))
+                        addEmail(mail);
+                    else System.out.println("Неправильный email");
+                    break;
+
+                default:
+                    continue;
+
+            }
+
+
+        }
+    }
+
+    public static void addEmail (String email){
+        if (!email.matches(".+@.+\\.(com|ru)")) {
+            System.out.println("Не правильно введен email");
+            return;
+        } else emailsTreeSet.add(email);
+    }
+
+    public static void printEmails () {
+        for (String item : emailsTreeSet) {
+            System.out.println(item);
+        }
+    }
+
+
+}
